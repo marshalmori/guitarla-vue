de5cea2c6136c16523029805c86a73807d42dc72
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-11-16 17:12:27 -0300

    Adicionando dados ao state com ref

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-import { ref, reactive } from "vue";
+import { ref, reactive, onMounted } from "vue";
 import { db } from "./data/guitarras";
 
-const state = reactive({
-  guitarras: db,
+const guitarras = ref([]);
+
+onMounted(() => {
+  guitarras.value = db;
 });
 </script>
 
