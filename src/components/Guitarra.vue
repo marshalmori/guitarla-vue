963c695bd547a2b30649591b97cf8994a1bdbdfb
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-11-17 22:16:03 -0300

    Detectando se o carrinho está vazio ou tem elementos adicionados

--- src/components/Guitarra.vue
+++ src/components/Guitarra.vue
@@ -29,7 +29,7 @@ defineEmits(["agregar-carrito"]);
       <button
         type="button"
         class="btn btn-dark w-100"
-        @click="$emit('agregarCarrito', { guitarra })"
+        @click="$emit('agregar-carrito', guitarra)"
       >
         Agregar al Carrito
       </button>
